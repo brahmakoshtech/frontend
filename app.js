44ab108556ca0c7c5954e29fbd5240bba633e6a8
brahmakoshtech <brahmakoshtech@gmail.com>
2026-01-10 13:12:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const server = http.createServer(app);
 
 // CORS Configuration - MUST BE BEFORE OTHER MIDDLEWARE
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174', 'https://backend-jfg8.onrender.com', 'https://brahmakoshfrontend.vercel.app'],
+  origin: ['http://localhost:5173', 'https://frontend-seven-steel-66.vercel.app', 'http://localhost:5174', 'https://backend-jfg8.onrender.com', 'https://brahmakoshfrontend.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
