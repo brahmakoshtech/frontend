14ac3f1b0f6a6e794594cc7a383c5989bc5bf484
Diintech7 <diintechteam7@gmail.com>
2026-03-30 12:52:13 +0530

    lets see

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ export default {
       if (host.includes('dash.brahmakosh.com') && (path === '/' || path === '')) {
         router.replace('/client/overview');
       }
+      if (host.includes('admin.brahmakosh.com') && (path === '/' || path === '')) {
+        router.replace('/admin/overview');
+      }
     });
 
     return () => <RouterView />;
